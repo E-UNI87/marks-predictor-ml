a970283d6a5620fbe7109532f2232542d5c12f1d
E-UNI87 <euni99901@gmail.com>
2026-02-05 19:27:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+df = pd.read_csv("students.csv")
+
+X = df[["Hours"]]
+y = df["Marks"]
+
+model = LinearRegression()
+model.fit(X, y)
+
+
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import streamlit as st
@@ -18,6 +31,24 @@ model.fit(X, y)
 # User input
 hours = st.number_input("Enter study hours:", min_value=0.0, step=0.5)
 
-if st.button("Predict Marks"):
+if st.button("🎯 Predict My Marks"):
     prediction = model.predict([[hours]])
-    st.success(f"Predicted Marks: {round(prediction[0], 2)}")
+    st.success(f"✅ Estimated Marks: {round(prediction[0], 2)} / 100")
+    # Prediction for all data (line)
+    predicted_all = model.predict(X)
+
+    # Create graph
+    fig, ax = plt.subplots()
+
+    ax.scatter(df["Hours"], df["Marks"], label="Actual Marks")
+    ax.plot(df["Hours"], predicted_all, label="ML Prediction Line")
+    ax.scatter(hours, prediction, marker="*", s=200, label="Your Prediction")
+
+    ax.set_xlabel("Study Hours")
+    ax.set_ylabel("Marks")
+    ax.set_title("📈 Study Hours vs Marks (ML Model)")
+    ax.legend()
+
+    # Show graph in Streamlit
+    st.pyplot(fig)
+s
